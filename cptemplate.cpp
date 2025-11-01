eb0c769fc28ffd01c7182b58d818f7a748f740fd
Manit Relan <manitrelan159@gmail.com>
2025-11-01 16:15:39 +0530

    CP template
    This file sets up a template for competitive programming solutions, including input/output redirection and a basic structure for the solution class.

---
+++ cptemplate.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortcuts
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+class Solution {
+public:
+    void solve() {
+        // Write your solution logic here
+    }
+};
+
+int main() {
+    fast_io;
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1; // number of test cases
+    // Uncomment below line if multiple test cases are given:
+    // cin >> t;
+
+    Solution obj;
+    while (t--) {
+        obj.solve();
+    }
+
+    return 0;
+}
